ced7451e2b07330feada0386dce575dfa70fae8c
sangavi12 <30531504+sangavi12@users.noreply.github.com>
2017-07-28 21:13:45 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class add
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a,g;
+if(a>0)
+{
+System.out.println("enter the pos no");
+}
+else
+{
+System.out.println("enter the negative no");
+}
+}
+}
